0dd7f908ba5dd58a255982bb2ecde6897c92432e
drvgogos <filipdragosandrei@yahoo.com>
2025-05-18 20:10:36 +0300

    Add DataTransferApp/Pages/WebInterfaceTransferPage.xaml.cs 1747588235521

---
+++ DataTransferApp/Pages/WebInterfaceTransferPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace DataTransferApp.Pages;
+
+public partial class WebInterfaceTransferPage : ContentPage
+{
+    public WebInterfaceTransferPage()
+    {
+        InitializeComponent();
+    }
+}
