8dc39b9558eb8660401fdc6538b2cb70f8919d43
drvgogos <filipdragosandrei@yahoo.com>
2025-05-18 20:10:43 +0300

    Add DataTransferApp/Pages/BluetoothTransferPage.xaml.cs 1747588242502

---
+++ DataTransferApp/Pages/BluetoothTransferPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace DataTransferApp.Pages;
+
+public partial class BluetoothTransferPage : ContentPage
+{
+    public BluetoothTransferPage()
+    {
+        InitializeComponent();
+    }
+}
