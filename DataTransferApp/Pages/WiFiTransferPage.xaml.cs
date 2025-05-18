27eb0eb82f16c6e0e0d423b2be3da170344e0e7b
drvgogos <filipdragosandrei@yahoo.com>
2025-05-18 20:10:39 +0300

    Add DataTransferApp/Pages/WiFiTransferPage.xaml.cs 1747588238997

---
+++ DataTransferApp/Pages/WiFiTransferPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace DataTransferApp.Pages;
+
+public partial class WiFiTransferPage : ContentPage
+{
+    public WiFiTransferPage()
+    {
+        InitializeComponent();
+    }
+}
