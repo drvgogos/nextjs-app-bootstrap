0726f2e015b1eeb631b46025d1adf082e33750bc
drvgogos <filipdragosandrei@yahoo.com>
2025-05-18 20:10:41 +0300

    Add DataTransferApp/Pages/USBTransferPage.xaml.cs 1747588240813

---
+++ DataTransferApp/Pages/USBTransferPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace DataTransferApp.Pages;
+
+public partial class USBTransferPage : ContentPage
+{
+    public USBTransferPage()
+    {
+        InitializeComponent();
+    }
+}
