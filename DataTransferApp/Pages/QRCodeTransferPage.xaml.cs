cb53adc0ff1f7fd21abfddc65050409b7696da4e
drvgogos <filipdragosandrei@yahoo.com>
2025-05-18 20:10:38 +0300

    Add DataTransferApp/Pages/QRCodeTransferPage.xaml.cs 1747588237267

---
+++ DataTransferApp/Pages/QRCodeTransferPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace DataTransferApp.Pages;
+
+public partial class QRCodeTransferPage : ContentPage
+{
+    public QRCodeTransferPage()
+    {
+        InitializeComponent();
+    }
+}
