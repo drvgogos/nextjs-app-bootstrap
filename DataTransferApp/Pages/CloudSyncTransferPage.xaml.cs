f494f3b04240aef69a4cb2eb8d801771271d8b43
drvgogos <filipdragosandrei@yahoo.com>
2025-05-18 20:10:33 +0300

    Add DataTransferApp/Pages/CloudSyncTransferPage.xaml.cs 1747588232627

---
+++ DataTransferApp/Pages/CloudSyncTransferPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace DataTransferApp.Pages;
+
+public partial class CloudSyncTransferPage : ContentPage
+{
+    public CloudSyncTransferPage()
+    {
+        InitializeComponent();
+    }
+}
